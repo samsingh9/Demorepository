d565ee41d80ef056a0b4a4192f3b25acbe8a0477
samsingh9 <167865695+samsingh9@users.noreply.github.com>
2025-02-13 13:54:03 +0530

    Add files via upload

---
+++ testingBaba_1April/src/main/java/baselibrary/Baselibrary.java
@@ -0,0 +1,17 @@
+package baselibrary;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class Baselibrary {
+
+	@Test
+	public void lauch()
+	{
+		
+		WebDriver driver = new ChromeDriver();
+	}
+}
